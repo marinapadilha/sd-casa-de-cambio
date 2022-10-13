c0454a870bd39e0f9442d589747f8724feda4a96
MarinaPadilha <marina.padilha27@gmail.com>
2022-10-13 17:59:15 -0300

    Adiciona regras.

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const clearList = () => {
   const currencyList = document.getElementById('currency-list');
   currencyList.innerHTML = '';
-}
+};
+
+const clearCurrencyInput = () => {
+  const currencyInputElement = document.getElementById('currency-input');
+  currencyInputElement.value = '';
+};
 
 const renderBaseCurrencyTitle = (base) => {
   const baseTitle = document.getElementById('title-base');
   baseTitle.innerHTML = `Valores referentes a: 1 ${base}`;
-}
+};
 
 const handleRateItemList = (currency, value) => {
   // 1. Recuperar a lista
@@ -24,7 +29,7 @@ const handleRateItemList = (currency, value) => {
   // 4. Inserir a li na ul
   currencyList.appendChild(li);
 
-}
+};
 const handleRates = (rates) => {
   const ratesEntries = Object.entries(rates);
   ratesEntries.forEach((entry) => {
@@ -36,11 +41,16 @@ const handleSearchEvent = async () => {
   const currencyElement = document.getElementById('currency-input');
   // console.log(currencyElement);
   const currencyValue = currencyElement.value;
+  if (currencyValue === '') {
+    alert('Preencha o campo de pesquisa!');
+    return;
+  }
   const object = await fetchExchangeRates(currencyValue);
 
   clearList();
   handleRates(object.rates);
   renderBaseCurrencyTitle(object.base);
+  clearCurrencyInput();
 };
 
 const setupHtmlElements = () => {
